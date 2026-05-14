19493f460afc22bbd7576cf7c36e410dae5377f1
bruno.bastos <77030205+brunoxbastos@users.noreply.github.com>
2026-05-14 17:57:21 +0000

    primeiro commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: '/estacao-vegana/',
+  build: {
+    rollupOptions: {
+      input: {
+        main:     resolve(__dirname, 'index.html'),
+        produtos: resolve(__dirname, 'produtos.html'),
+        cesta:    resolve(__dirname, 'cesta.html'),
+        login:    resolve(__dirname, 'login.html'),
+      },
+    },
+  },
+})
